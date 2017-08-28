d2b6a494306e0e4058adf343f802889a39b1c7d5
Guillaume Lessard <dhtnstff@gmail.com>
2017-08-28 13:28:01 -0600

    Swift 4.0 update

--- Package.swift
+++ Package.swift
@@ -1,5 +1,26 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
+let name = "async"
+
+#if swift(>=4.0)
+
 let package = Package(
-name: "async"
+  name: name,
+  products: [
+  .library(name: name, targets: [name]),
+  ],
+  targets: [
+    .target(name: name),
+  ],
+  swiftLanguageVersions: [3,4]
 )
+
+#else
+
+let package = Package(
+  name: name
+)
+
+#endif
